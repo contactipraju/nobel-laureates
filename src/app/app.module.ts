fc2853977569ca5d040e9ad0ce07eb746c87a8e1
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-05-16 16:10:44 +1000

    Adding the main component to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { NgxBootstrapSliderModule } from 'ngx-bootstrap-slider';
+import { NobelsComponent } from './nobels/nobels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NobelsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nobels/nobels.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nobels',
+  templateUrl: './nobels.component.html',
+  styleUrls: ['./nobels.component.sass']
+})
+export class NobelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
