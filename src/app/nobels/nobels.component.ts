585ac92764cdf2f3751349626381e9afaa6163bf
Prasada Raju Indukuri <18624614+contactipraju@users.noreply.github.com>
2019-05-16 22:13:33 +1000

    Revert "Auto-generated commit"

---
+++ src/app/nobels/nobels.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { NobelsService }     from './nobels.service';
+import { Laureate }          from './nobels.model';
+
+@Component({
+  selector: 'app-nobels',
+  templateUrl: './nobels.component.html',
+  styleUrls: ['./nobels.component.sass']
+})
+export class NobelsComponent implements OnInit {
+
+  constructor(public nobelsService: NobelsService) {
+  }
+
+  laureates: Laureate[];
+  laureatesLoaded: boolean = false;
+
+  ngOnInit() {
+    this.loadLaureates();
+  }
+
+  loadLaureates() {
+    this.nobelsService.getLocalLaureates().subscribe(result => {
+      this.laureates = result;
+      this.laureatesLoaded = true;
+    });
+  }
+}
