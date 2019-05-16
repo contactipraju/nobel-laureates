57495f62c151d47778f9ed2b72b1b1b75c92d9c8
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-05-16 16:29:14 +1000

    Minor cleanup; some documentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+/* Angular Modules */
 import { NgModule }         from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule }     from '@angular/router';
 import { BrowserModule }    from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+/* 3rd-party UI Components */
+import { BsDatepickerModule }       from 'ngx-bootstrap/datepicker';
 import { NgxBootstrapSliderModule } from 'ngx-bootstrap-slider';
+
+/* Application Components */
+import { AppComponent }     from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+/* Components */
 import { NobelsComponent } from './nobels/nobels.component';
 
 @NgModule({
@@ -16,12 +22,13 @@ import { NobelsComponent } from './nobels/nobels.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     NgxBootstrapSliderModule,
     BsDatepickerModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: NobelsComponent, pathMatch: 'full' },
-      { path: 'nobels', component: NobelsComponent },
+      { path: 'nobels', component: NobelsComponent }
     ])
   ],
   providers: [],
