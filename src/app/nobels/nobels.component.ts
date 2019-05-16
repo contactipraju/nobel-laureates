914aa91f6532ffe6b7a25993a710b2cda1a695fc
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-05-16 19:18:09 +1000

    Using NobelsService to load the Laureates from the local JSON

--- src/app/nobels/nobels.component.ts
+++ src/app/nobels/nobels.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NobelsService }     from './nobels.service';
+import { Laureates }         from './nobels.model';
 
 @Component({
   selector: 'app-nobels',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NobelsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public nobelsService: NobelsService) {
   }
 
+  laureates: Laureates[];
+  laureatesLoaded: boolean = false;
+
+  ngOnInit() {
+    this.loadLaureates();
+  }
+
+  loadLaureates() {
+    this.nobelsService.getLocalLaureates().subscribe(result => {
+      this.laureates = result;
+      this.laureatesLoaded = true;
+    });
+  }
 }
