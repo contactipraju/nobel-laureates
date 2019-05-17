ce01f41d23de2564ebaea2024448c16b2c1c8ebe
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-05-17 10:38:03 +1000

    That renders all filters (children), keeps track of current state of filters, broadcasts reset messages etc

---
+++ src/app/nobels/nobel-filter/nobel-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nobel-filter',
+  templateUrl: './nobel-filter.component.html',
+  styleUrls: ['./nobel-filter.component.scss']
+})
+export class NobelFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
