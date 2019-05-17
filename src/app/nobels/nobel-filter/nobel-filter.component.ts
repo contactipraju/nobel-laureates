c5eafe6388863e3075168e223b9f6fd1c720f0b3
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-05-17 11:09:08 +1000

    Importing Filter model, Filter service and required core components to Emit events; Added high-level functions

--- src/app/nobels/nobel-filter/nobel-filter.component.ts
+++ src/app/nobels/nobel-filter/nobel-filter.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }    from '@angular/core';
+import { EventEmitter, Output } from '@angular/core';
+
+import { Subject } from 'rxjs';
+
+import { Filter }             from './nobel-filter.model';
+import { NobelFilterService } from './nobel-filter.service';
 
 @Component({
   selector: 'app-nobel-filter',
   templateUrl: './nobel-filter.component.html',
-  styleUrls: ['./nobel-filter.component.scss']
+  styleUrls: ['./nobel-filter.component.scss'],
+  providers: [NobelFilterService]
 })
 export class NobelFilterComponent implements OnInit {
+  private searchUpdated = new Subject<Filter>();
+  @Output() searchTextEmitter = new EventEmitter<Filter>();
 
-  constructor() { }
+  filterInputs = new Filter();
+  allFilters = this.filterInputs.allOptions();
+
+  constructor(private filterService: NobelFilterService) {
+  }
 
   ngOnInit() {
   }
 
+  filterEventHandler($event) {
+    console.log("filterEventHandler", $event);
+  }
+
+  onSearchChange() {
+    console.log("onSearchChange");
+  }
+
+  onResetFilters() {
+    console.log("onResetFilters");
+  }
+
+  emitFilters() {
+    console.log("emitFilters");
+  }
 }
