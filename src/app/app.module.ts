5e6cef1bdbb83c9e9e23e888011449259d3f4889
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-05-17 11:57:26 +1000

    A button-group component that renders and lets user select one out of many options (Like a Radio Button); multi-select mode also supported; images can be placed; can also configure number of buttons to be shown in a row

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule }         from '@angular/core';
 import { RouterModule }     from '@angular/router';
 import { BrowserModule }    from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }      from '@angular/forms';
 
 /* 3rd-party UI Components */
 import { BsDatepickerModule }       from 'ngx-bootstrap/datepicker';
 import { NgxBootstrapSliderModule } from 'ngx-bootstrap-slider';
+import { ButtonsModule }            from 'ngx-bootstrap/buttons';
 
 /* Application Components */
 import { AppComponent }     from './app.component';
@@ -21,6 +23,7 @@ import { NobelsComponent }       from './nobels/nobels.component';
 import { LaureateCardComponent } from './nobels/laureate-card/laureate-card.component';
 import { NobelPrizeComponent }   from './nobels/laureate-card/nobel-prize/nobel-prize.component';
 import { NobelFilterComponent }  from './nobels/nobel-filter/nobel-filter.component';
+import { ButtonGroupComponent }  from './nobels/nobel-filter/button-group/button-group.component';
 
 @NgModule({
   declarations: [
@@ -28,13 +31,16 @@ import { NobelFilterComponent }  from './nobels/nobel-filter/nobel-filter.compon
     NobelsComponent,
     LaureateCardComponent,
     NobelPrizeComponent,
-    NobelFilterComponent
+    NobelFilterComponent,
+    ButtonGroupComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule,
     NgxBootstrapSliderModule,
+    ButtonsModule.forRoot(),
     BsDatepickerModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: NobelsComponent, pathMatch: 'full' },
