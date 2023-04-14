ace3b97c8a635329788342a33ae19eafb2dbc17c
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-04-14 18:56:12 +1000

    fixing the wrong project to deploy app in firebase hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false
+	production: false,
+
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+	firebase: {
+		apiKey: "AIzaSyDY_HmF74O6gFzJ6r7UEmu4uuGAGBLcGnE",
+		authDomain: "nobel-laureates-360.firebaseapp.com",
+		projectId: "nobel-laureates-360",
+		storageBucket: "nobel-laureates-360.appspot.com",
+		messagingSenderId: "533447895626",
+		appId: "1:533447895626:web:1eabfa0b0af13728d254b3",
+		measurementId: "G-9MF97KB5E5"
+	}
 };
 
 /*
